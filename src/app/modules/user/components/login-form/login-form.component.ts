e9161842584f5ab7f3daf2adf8bbddae6fa4415c
mechztcc <dojathan@gmail.com>
2022-03-23 21:33:20 -0300

    feat: added some features in login form

--- src/app/modules/user/components/login-form/login-form.component.ts
+++ src/app/modules/user/components/login-form/login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,13 +9,32 @@ import { Router } from '@angular/router';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  isVisible: boolean = false;
+
+  form: FormGroup;
+
+  constructor(private router: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initForm();
   }
+
+  initForm() {
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  validateForm() {}
 
   navigate() {
     this.router.navigate(['user/create-account']);
   }
+
+  changeVisibility() {
+    this.isVisible = !this.isVisible;
+  }
+
 
 }
