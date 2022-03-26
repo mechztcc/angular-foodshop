a872de7a9caf277d6e266a6812934d98b45674dc
mechztcc <dojathan@gmail.com>
2022-03-26 10:31:38 -0300

    feat: created dashboard module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule)},
+  { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)},
 ];
 
 @NgModule({
