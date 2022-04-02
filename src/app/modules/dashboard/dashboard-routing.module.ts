66a7796ad1c8131896bf08dce26f10116896d41b
mechztcc <dojathan@gmail.com>
2022-04-02 12:08:58 -0300

    feat: added auth guard to some pages

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardGuard } from '../shared/core/guards/auth-guard.guard';
 import { HomeComponent } from './pages/home/home.component';
 
 
 const routes: Routes = [
-  { path: 'dashboard', component: HomeComponent }
+  { path: 'dashboard', component: HomeComponent, canActivate: [AuthGuardGuard] }
 ];
 
 @NgModule({
--- src/app/modules/shared/core/guards/auth-guard.guard.ts
+++ src/app/modules/shared/core/guards/auth-guard.guard.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuardGuard implements CanActivate {
+  /**
+   *
+   */
+  constructor(private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return false;
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      this.router.navigate(['user/login']);
+      return false;
+    } else {
+      return true;
+    }
   }
-  
 }
